cfdc843a2f9efc4741feb3104421918baa24bf06
Eksith Rodrigo <eksith@live.com>
2016-03-26 17:34:06 -0400

    Added files via upload

---
+++ templates/admin/manage_settings.js
@@ -0,0 +1,30 @@
+$boot(function() {
+	// Enable elements to be shown after page load
+	$each( $all('.showload'), function(e, i, t) {
+		$toggle(e, 'showload');
+	});
+	
+	$each( $all('.hideload'), function(e, i, t) {
+		$toggle(e, 'hideload');
+	});
+	
+	var 
+	ns	= 2000,
+	nh	= function(){
+			$each( $all('nav.showload'), function(al) {
+				$toggle(al, 'showload');
+			});
+		},
+	nht	= setTimeout(nh, ns);
+	
+	$each( $all('nav.showload'), function(al) {
+		$on( al, 'mouseover', function() {
+			clearTimeout( nht );
+		})
+		$on( al, 'mouseout', function() {
+			al.classList.remove('showload');
+			al.className += ' showload';
+			nht = setTimeout( nh, ns);
+		});
+	});
+});
